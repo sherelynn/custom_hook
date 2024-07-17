9fd29cdc59c96e30b10dfc1b924ec18e6ecb7ba5
Sherelynn Baguioso <sherelynn.b@gmail.com>
2024-07-17 17:30:05 +1200

    Fetch data using custom hook

--- src/Components/FetchData.jsx
+++ src/Components/FetchData.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import UseFetch from './UseFetch'
+import './FetchData.css'
 
 const FetchData = () => {
+  const [data] = UseFetch('https://api.npoint.io/9045c260b1565daa9e15')
+  console.log(data)
+
   return (
     <>
-     <ul className='list_data_main'>
-        <h1 className='usefetch_heading'>Use Fetch Custom Hook</h1>
-     </ul>
+      <ul className="list_data_main">
+        <h1 className="usefetch_heading">Use Fetch Custom Hook</h1>
+      </ul>
     </>
   )
 }
