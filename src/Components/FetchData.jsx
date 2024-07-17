96ed9da591ecdd7d73b6dcc49c61686c1735ec1b
Sherelynn Baguioso <sherelynn.b@gmail.com>
2024-07-17 17:30:59 +1200

    Retrieve data in front end

--- src/Components/FetchData.jsx
+++ src/Components/FetchData.jsx
@@ -10,6 +10,24 @@ const FetchData = () => {
     <>
       <ul className="list_data_main">
         <h1 className="usefetch_heading">Use Fetch Custom Hook</h1>
+        {data &&
+          data.map((item) => (
+            <>
+              <li className="list_data">
+                <h3 className="item_name">{item.name}</h3>
+                <p>
+                  <span>Benefits: </span> {item.benefits}
+                </p>
+                <p>
+                  <span>Best time to eat: </span> {item.best_time_to_intake}
+                </p>
+                <p>
+                  <span>Importance: </span>
+                  {item.importance}
+                </p>
+              </li>
+            </>
+          ))}
       </ul>
     </>
   )
